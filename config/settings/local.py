e2d421de3f1aacf633519030bd12a29a22c41522
Marmik Patel <marmikpatel250@gmail.com>
2022-02-14 19:04:25 +0530

    .

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="hQhv1cl2fuIO2notRX29NknhL9culI3IpgmoB1NRag8sY8tK1XFVAZLJcX97PgfL",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "64.227.185.42"]
 
 # CACHES
 # ------------------------------------------------------------------------------
@@ -48,7 +48,7 @@ DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TEMPLATE_CONTEXT": True,
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "64.227.185.42"]
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
 
 # django-extensions
