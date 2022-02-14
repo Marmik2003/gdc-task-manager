8eaf31fabe55b3f5bc84158b7a65ca9187285b4d
Marmik Patel <marmikpatel250@gmail.com>
2022-02-14 18:59:36 +0530

    ..

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, User
 from django.db import transaction
 from django import forms
 
-from tasks.models import UserTaskReportSetting
+from task_manager.tasks.models import UserTaskReportSetting
 
 
 class UserLoginForm(AuthenticationForm):
